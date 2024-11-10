89fb9f530a9ebf71727f101ee5dae6e161a75788
Mahmmoud Elkhateb <98628050+PepoRoot@users.noreply.github.com>
2024-11-10 19:37:16 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+  const message = document.getElementById("message");
+  message.innerText = "Cybersecurity is not just a career; it's a passion and a commitment to protect the digital world!";
+}
